2082906117cd2f99d194950b4bc9548e6aa54219
João Luiz de Castro <joo.luiz.98@outlook.com>
2021-04-02 11:06:43 -0300

    implementing redux, react-redux and adding a provider to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import Header from "./components/commom/Header"
 
+import {Provider} from 'react-redux'
+import store from './store'
+
 class App extends Component{
   render(){
     return (
-      <Fragment>
-        <Header />
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+        </Fragment>
+      </Provider>
     )
   }
 }
